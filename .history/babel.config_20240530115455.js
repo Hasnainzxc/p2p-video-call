671ff04a0edf3bde557f58b065b8c4b1a0fd1de3
hasnainzxc <analogue00420@gmail.com>
2024-06-02 12:20:31 +0500

    updated node packages

--- .history/babel.config_20240530115455.js
+++ .history/babel.config_20240530115455.js
@@ -2,7 +2,7 @@ module.exports = {
   presets: ["react-app"],
   plugins: ["@babel/plugin-transform-private-property-in-object"],
 };
-// module.export = {
-//     presets ["resct-start"],
-//     this.plugins{"eslint/pluglin /redux.eslint"}
-// }
+module.export = {
+    presets ["resct-start"],
+    this.plugins{"eslint/pluglin /redux"}
+}
